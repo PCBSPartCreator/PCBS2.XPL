99b95b1e5deba69aba2135b2c288f6cf591428b3
anonymus637 <2+anonymus637@noreply.localhost>
2026-05-13 20:10:14 +0200

    Delete directory 'PCBS2.XPL'

--- PCBS2.XPL/hooks.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-#include "config.h"
-
-bool Hooks_Install();
-void Hooks_SetPendingMods(std::vector<ModPart>* mods);
--- PCBS2.XPL/logger.cpp
+++
@@ -1,4 +0,0 @@
-#include "logger.h"
-
-std::ofstream Logger::s_file;
-std::mutex    Logger::s_mutex;
