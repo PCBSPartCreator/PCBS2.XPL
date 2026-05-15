d8101058545b1cf935be7ce4dc82bb102ff83c8c
anonymus637 <2+anonymus637@noreply.localhost>
2026-05-15 23:05:19 +0200

    Dateien nach "PCBS2.XPL" hochladen

--- PCBS2.XPL/config.h
+++ PCBS2.XPL/config.h
@@ -2,11 +2,9 @@
 #include <string>
 #include <vector>
 
-struct ModPart {
-    std::string partType;
-    std::string id;
+struct ModFile {
+    std::string fullPath;
     std::string fileName;
-    std::vector<std::pair<std::string, std::string>> properties;
 };
 
-std::vector<ModPart> Config_LoadMods(const std::string& gameDir);
+std::vector<ModFile> Config_ScanMods(const std::string& gameDir);
